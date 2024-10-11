0dccc95fbb8d5f7ca924332e837b311b7bcef987
Shiv <shivrajdesai222@gmail.com>
2024-10-11 18:38:17 +0530

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,39 +3,19 @@ import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
 
 function Home() {
-  return (
-    <>
-       <h1 className='text-center text-2xl font-bold'>Be The Member of our Community</h1>
-   
-    <div className='w-full h-screen bg-zinc-800 flex items-center justify-center' >
-       
-       <ThemeProvider
-      theme={{
-        palette: {
-          primary: {
-            main: '#007FFF',
-            dark: '#0066CC',
-          },
-        },
-      }}
-    >
-      <Box
-        sx={{
-            width: "80vw",
-            height: "80vh",
-            borderRadius: 1,
-            bgcolor: 'primary.main',
-            '&:hover': {
-                bgcolor: 'primary.dark',
-            },
-        }}
-        />
-      
-    </ThemeProvider>
-    
-    </div>
-    </>
-  )
+    return (
+        <>
+            <h1 className='text-center text-2xl font-bold'>Be The Member of our Community</h1>
+
+            <div className='w-full h-screen bg-zinc-800 flex items-center justify-center' >
+
+
+                <div className='w-[80vw] h-[85vh] bg-slate-300 rounded-lg'>
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home
